ed60d2e3104fd34b6c6a67a5dba2a41cd0829ea1
Saeed Rafay <saeedahmadrafay@hotmail.com>
2021-03-24 23:58:06 +0100

    search field placeholder added

--- week3/project/hackyourweather/src/SearchWeather.js
+++ week3/project/hackyourweather/src/SearchWeather.js
@@ -2,7 +2,7 @@ function SearchWeather(props) {
   const { searchTerm, setSearchTerm, setSearchBtn } = props;
   return (
     <div className="searchWeather">
-      <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+      <input type="text" placeholder="Search City" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
       <button onClick={() => setSearchBtn(searchTerm)}>Search</button>
     </div>
   );
